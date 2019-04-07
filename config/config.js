15b1969ff37fe2f28c6fb939838022d52abe93b4
Ed <ed@polarity.io>
2019-04-06 21:37:15 -0400

    Reformat

--- config/config.js
+++ config/config.js
@@ -1,12 +1,11 @@
-// EPOCH Time Converter Config File
 module.exports = {
-	name: "EPOCHTimeConverter",
-	acronym: "TIME",
-	description: "Converts EPOCH Time to Human Readable Date",
-	customTypes:[
-        	{
-            		key: 'unixtime',
-            		regex: /\s\d{10}(?!\w)/
-        	}
-	]
+  name: 'EPOCHTimeConverter',
+  acronym: 'TIME',
+  description: 'Converts EPOCH Time to Human Readable Date',
+  customTypes: [
+    {
+      key: 'unixtime',
+      regex: /\s\d{10}(?!\w)/
+    }
+  ]
 };
